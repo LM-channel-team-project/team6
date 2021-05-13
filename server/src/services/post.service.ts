a2f77778e05b9b305fd95869940d4dc11e237301
sewoop <raku@hotmail.co.kr>
2021-05-14 03:47:59 +0900

    feat: Define post service layer (#63)

---
+++ server/src/services/post.service.ts
@@ -0,0 +1,18 @@
+import { getRepository } from "typeorm";
+import { Post } from "@models/entity/Post";
+import * as custom from "@utils/custom";
+
+// Get Posts
+export const findAll = async () => {};
+
+// Get Post
+export const findOne = async () => {};
+
+// Create Post
+export const createPost = async () => {};
+
+// Update Post
+export const updatePost = async () => {};
+
+// Delete Post
+export const deletePost = async () => {};
