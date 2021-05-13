55a2fc4b036b509058039072a3c79dd3ebb2c20b
sewoop <raku@hotmail.co.kr>
2021-05-14 03:56:17 +0900

    feat: Define post filter, clicked function (#63)

--- server/src/services/post.service.ts
+++ server/src/services/post.service.ts
@@ -3,10 +3,10 @@ import { Post } from "@models/entity/Post";
 import * as custom from "@utils/custom";
 
 // Get Posts
-export const findAll = async () => {};
+export const findPosts = async () => {};
 
 // Get Post
-export const findOne = async () => {};
+export const findPost = async () => {};
 
 // Create Post
 export const createPost = async () => {};
@@ -16,3 +16,15 @@ export const updatePost = async () => {};
 
 // Delete Post
 export const deletePost = async () => {};
+
+// Like Clicked
+export const clickLikePost = async () => {};
+
+// Order by Week
+export const filterWeekPost = async () => {};
+
+// Order by Latest
+export const filterLatestPost = async () => {};
+
+// Order by Like
+export const filterLikePost = async () => {};
