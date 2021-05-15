d38e1c0bff483f85638349411ea55eaf63962daf
sewoop <raku@hotmail.co.kr>
2021-05-16 03:39:04 +0900

    feat: Post click Like API (#52)

--- server/src/routes/post.router.ts
+++ server/src/routes/post.router.ts
@@ -5,11 +5,12 @@ import { PostController } from "@controllers/v1/";
 const router = express.Router();
 
 // Post filter (QS=week, latest)
-router.get("/:id", isAuthenticated, PostController.findPost);
+router.patch("/like/:id", PostController.clickLikePost);
+router.get("/:id", PostController.findPost);
 router.patch("/:id", isAuthenticated, PostController.updatePost);
 router.delete("/:id", isAuthenticated, PostController.deletePost);
 
 router.post("/", isAuthenticated, PostController.createPost);
-router.get("/", isAuthenticated, PostController.findPosts);
+router.get("/", PostController.findPosts);
 
 export default router;
