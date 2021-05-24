50f07a5751fc396dc459e2af5172e3ff1a3595ae
sewoop <raku@hotmail.co.kr>
2021-05-25 02:48:26 +0900

    feat: JWT Routes 수정 (#77)

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -5,32 +5,31 @@ import { AuthController } from "@controllers/v1";
 
 const router = express.Router();
 
-// OAuth
-// - Kakao
+// Oauth - Kakao
 router.get(
   "/oauth/kakao",
   passport.authenticate("kakao", { scope: ["profile"], session: false }),
 );
-router.get("/oauth/kakao/callback", AuthController.kakaoCallbacks);
+router.get("/oauth/kakao/callback", AuthController.authKakaoLogin);
 
-// - Github
+// Oauth - Github
 router.get(
   "/oauth/github",
   passport.authenticate("github", { scope: ["profile"], session: false }),
 );
-router.get("/oauth/github/callback", AuthController.githubCallbacks);
+router.get("/oauth/github/callback", AuthController.authGithubLogin);
 
 // Local
 // - Login, SignUp, Logout
-router.get("/logout", AuthController.logout);
-router.post("/signup", AuthController.createUser);
-router.post("/", AuthController.localCallbacks);
+router.get("/logout", verificationUser, AuthController.authLogout);
+router.post("/signin", AuthController.authLocalLogin);
+router.post("/signup", AuthController.authCreateUser);
 
 // - User Update, User Delete, Find One User
-router.get("/:id", verificationUser, AuthController.findUser);
-router.patch("/:id", verificationUser, AuthController.updateUserGeneral);
-router.delete("/:id", verificationUser, AuthController.deleteUser);
+router.get("/:id", verificationUser, AuthController.authFindOne);
+router.patch("/:id", verificationUser, AuthController.authUpdateUserGeneral);
+router.delete("/:id", verificationUser, AuthController.authDeleteUser);
 
-router.get("/", verificationUser, AuthController.findUsers);
+router.get("/", verificationUser, AuthController.authFindAll);
 
 export default router;
--- server/src/routes/post.router.ts
+++ server/src/routes/post.router.ts
@@ -1,30 +1,29 @@
 import express from "express";
-import {
-  isAuthenticated,
-  isNotAuthenticated,
-} from "@middlewares/authenticate.middleware";
 import verificationUser from "@middlewares/jwtverification.middleware";
-import { PostController } from "@controllers/v1/";
+import verifyPostOwner from "@middlewares/verifyPostOwner.middleware";
+import { PostController } from "@controllers/v1";
 
 const router = express.Router();
 
 // Post filter (QS=week, latest)
-router.patch("/like/:id", verificationUser, PostController.clickLikePost);
-router.get("/:id", verificationUser, PostController.findPost);
+router.patch(
+  "/like/:id",
+  [verificationUser, verifyPostOwner],
+  PostController.clickLikePost,
+);
+
 router.patch(
   "/:id",
-  isAuthenticated,
-  verificationUser,
+  [verificationUser, verifyPostOwner],
   PostController.updatePost,
 );
 router.delete(
   "/:id",
-  isAuthenticated,
-  verificationUser,
+  [verificationUser, verifyPostOwner],
   PostController.deletePost,
 );
-
-router.post("/", isAuthenticated, verificationUser, PostController.createPost);
+router.get("/:id", verificationUser, PostController.findPost);
+router.post("/", verificationUser, PostController.createPost);
 router.get("/", verificationUser, PostController.findPosts);
 
 export default router;
