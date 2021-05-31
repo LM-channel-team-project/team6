27a9e0395e6a59881943569bc10b63f9cb3a0d44
AEJIJEON <jeonaeji@gist.ac.kr>
2021-06-01 00:44:46 +0900

    fix: header error in Detail Page #98

--- client/src/components/atoms/DropDown/style.ts
+++ client/src/components/atoms/DropDown/style.ts
@@ -31,6 +31,7 @@ export const DropDown = styled.div`
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 
   text-align: center;
+  background-color: white;
 `;
 export const DropDownContent = styled.div`
   ${(props: DropDownContentProps) =>
